8edb05cb3784bf6169d2b1aadb89fe2ead63ccf9
ezioa12 <soewunna12@gmail.com>
2021-02-05 13:39:16 +0630

    some init msg

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+@extends('master')
+@section("content")
+<div class="container "></div>
+    <div class="row custom-login">
+        <div class="col-sm-4 col-sm-offset-4" >
+            <form action="">
+            <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+<div class="mb-3">
+  <label for="formFile" class="form-label">Default file input example</label>
+  <input class="form-control" type="file" id="formFile">
+</div>
+<div class="mb-3">
+  <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+  <input class="form-control" type="file" id="formFileMultiple" multiple>
+</div>
+<button class="btn btn-primary">Click on me!!</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
